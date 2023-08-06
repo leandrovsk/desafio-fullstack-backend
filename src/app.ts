6ff5a44f641bcf929d34994a30727950e957403e
leandro <contas.center@gmail.com>
2023-08-06 12:03:49 -0300

    fix: cors error

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,14 @@ import { loginRoutes } from "./routes/login.routes"
 import userRoutes from "./routes/users.routes"
 import { handleAppError } from "./middlewares/handleAppError.middleware"
 import { contactsRoutes } from "./routes/contacts.routes"
+import cors from "cors"
 
 
 const app: Application = express()
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 app.use("/login", loginRoutes)
 app.use("/users", userRoutes)
 app.use("/contacts", contactsRoutes)
