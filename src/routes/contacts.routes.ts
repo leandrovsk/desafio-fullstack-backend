ad0f87b5bbe4a48b7171d01602f9620e9db83075
leandro <contas.center@gmail.com>
2023-08-06 23:53:16 -0300

    fix: email middleware in contacts

--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -2,17 +2,34 @@ import { Router } from "express";
 import { ensureTokenIsValidMiddleware } from "../middlewares/ensureTokenIsValid.middleware";
 import { ensureDataIsValid } from "../middlewares/ensureDataIsValid.middleware";
 import { contactSchemaRequest, contactSchemaUpdate } from "../schemas/contacts.schemas";
-import { createContactController, deleteContactController, listContactController, updateContactController } from "../controllers/contacts.controller";
+import {
+  createContactController,
+  deleteContactController,
+  listContactController,
+  updateContactController,
+} from "../controllers/contacts.controller";
 import { ensureUserIsOwnerMiddleware } from "../middlewares/ensureUserIsOwner.middleware";
 import { ensureEmailNotExistsMiddleware } from "../middlewares/ensureEmailNotExists.middleware";
 import { ensureContactIdExistsMiddleware } from "../middlewares/ensureContactIdExists.middleware";
 
+const contactsRoutes = Router();
 
-const contactsRoutes = Router()
+contactsRoutes.post("", ensureTokenIsValidMiddleware, ensureDataIsValid(contactSchemaRequest), createContactController);
+contactsRoutes.get("", ensureTokenIsValidMiddleware, listContactController);
+contactsRoutes.patch(
+  "/:id",
+  ensureTokenIsValidMiddleware,
+  ensureContactIdExistsMiddleware,
+  ensureUserIsOwnerMiddleware,
+  ensureDataIsValid(contactSchemaUpdate),
+  updateContactController
+);
+contactsRoutes.delete(
+  "/:id",
+  ensureTokenIsValidMiddleware,
+  ensureContactIdExistsMiddleware,
+  ensureUserIsOwnerMiddleware,
+  deleteContactController
+);
 
-contactsRoutes.post("", ensureTokenIsValidMiddleware, ensureDataIsValid(contactSchemaRequest), ensureEmailNotExistsMiddleware, createContactController)
-contactsRoutes.get("", ensureTokenIsValidMiddleware, listContactController)
-contactsRoutes.patch("/:id", ensureTokenIsValidMiddleware, ensureContactIdExistsMiddleware, ensureUserIsOwnerMiddleware, ensureDataIsValid(contactSchemaUpdate), ensureEmailNotExistsMiddleware, updateContactController)
-contactsRoutes.delete("/:id", ensureTokenIsValidMiddleware, ensureContactIdExistsMiddleware, ensureUserIsOwnerMiddleware, deleteContactController)
-
-export { contactsRoutes }
+export { contactsRoutes };
